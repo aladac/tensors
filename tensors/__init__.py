b7e85129b15b5fc2292d8c8e2ec6917a50023457
Adam Ladachowski <adam.ladachowski@gmail.com>
2026-02-19 05:37:23 +0100

    Version 0.1.18+9883929

--- tensors/__init__.py
+++ tensors/__init__.py
@@ -1,6 +1,6 @@
 """tsr: Read safetensor metadata, search and download CivitAI models."""
 
-__version__ = "0.1.18+f07a1e6"
+__version__ = "0.1.18+9883929"
 
 from tensors.cli import main
 from tensors.config import (
